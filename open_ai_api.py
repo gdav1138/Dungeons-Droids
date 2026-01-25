c54fdeff0c9065ff75874a568efcce2b5c6a81e7
Samuel Strelitz <strelits@oregonstate.edu>
2026-01-25 14:51:30 -0800

    Swapped to gemini

--- open_ai_api.py
+++ open_ai_api.py
@@ -6,18 +6,18 @@ import os
 from google import genai
 
 load_dotenv()
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+# client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+# def call_ai(request_text):
+#     return client.responses.create(
+#         model="gpt-5-nano",
+#         #model="gpt-5.2-chat-latest",
+#         input=request_text,
+#     ).output_text
+
+client = genai.Client()
 
 def call_ai(request_text):
-    return client.responses.create(
-        model="gpt-5-nano",
-        #model="gpt-5.2-chat-latest",
-        input=request_text,
-    ).output_text
-
-# client = genai.Client()
-#
-# def call_ai(request_text):
-#     response = client.models.generate_content(
-#         model="gemini-2.5-flash-lite", contents=request_text)
-#     return response.text
+    response = client.models.generate_content(
+        model="gemini-2.5-flash-lite", contents=request_text)
+    return response.text
